82177d8e1715eee1d583e9314be950bd0f00ac69
Marco Forgione <marco.forgione1986@gmail.com>
2025-02-06 19:06:42 +0100

    linear identification fixed

--- linear_identification.m
+++ linear_identification.m
@@ -1,19 +1,22 @@
 clear;
 clc;
-load bw_matlab
+load(fullfile('bwdataset', 'bw_matlab'))
 nx = 3;
 
+%% Train linear model
 train_ds = iddata(y, u, 1.0);
 %model = ssest(train_ds, nx,'Ts',1,'Feedthrough',1,'Focus','simulation'); 
 model = n4sid(train_ds, nx);
 
+%% Test linear model
+% On the same training data
 y_train_hat = compare(model, train_ds).OutputData;
 train_rmse = rms(train_ds.OutputData - y_train_hat) * 1e5; % 16.67
 train_fit = (1 - rms(train_ds.OutputData - y_train_hat)/rms(train_ds.OutputData))*100; 
 
-load(fullfile('Test signals', 'Validation signals', 'uval_multisine.mat'))
-load(fullfile('Test signals', 'Validation signals', 'yval_multisine.mat'))
-
+% On the test set
+load(fullfile('bwdataset', 'uval_multisine.mat'))
+load(fullfile('bwdataset', 'yval_multisine.mat'))
 yval_multisine = yval_multisine(:);
 uval_multisine = uval_multisine(:);
 
