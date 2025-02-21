02dea2c35d0341a088d9328c2513006181d8c482
bemporad <abemporad@gmail.com>
2025-02-21 19:11:28 +0100

    Minor changes

--- jax_sysid_boucwen.py
+++ jax_sysid_boucwen.py
@@ -16,16 +16,11 @@ from joblib import cpu_count
 
 plotfigs = True  # set to True to plot figures
 
-# Data generation
-seed = 3  # for reproducibility of results
-np.random.seed(seed)
-
 nx = 3  # number of states
 ny = 1  # number of outputs
 nu = 1  # number of inputs
 hidden_f = 16 # number of neurons in state-update function
 hidden_g = 16 # number of neurons in output function
-
 
 data_folder = "bwdataset"
 data_folder = Path(data_folder)
@@ -48,18 +43,15 @@ jax.config.update('jax_platform_name', 'cpu')
 if not jax.config.jax_enable_x64:
     jax.config.update("jax_enable_x64", True)  # Enable 64-bit computations
 
-
 @jax.jit
 def state_fcn(x, u, params):
     A, B, C, W1, W2, W3, b1, b2, W4, W5, b3, b4 = params
     return A@x+B@u+W3@nn.gelu(W1@x+W2@u+b1)+b2
 
-
 @jax.jit
 def output_fcn(x, u, params):
     A, B, C, W1, W2, W3, b1, b2, W4, W5, b3, b4 = params
     return C@x+W5@nn.gelu(W4@x+b3)+b4
-
 
 model = Model(nx, ny, nu, state_fcn=state_fcn, output_fcn=output_fcn)
 
@@ -93,9 +85,9 @@ def init_fcn(seed):
     return [A, B, C, W1, W2, W3, b1, b2, W4, W5, b3, b4]
 
 # initialize model coefficients
-#model.init(init_fcn(seed=0))
+#model.init(init_fcn(seed=35))
 # L2-regularization on initial state and model coefficients
-model.loss(rho_x0=1.e-8, rho_th=1.e-12)
+model.loss(rho_x0=1.e-8, rho_th=1.e-12, tau_th=0.*1.e-3)
 # number of epochs for Adam and L-BFGS-B optimization
 model.optimization(adam_epochs=200, lbfgs_epochs=5000)
 
@@ -115,5 +107,5 @@ R2, R2_test, msg = compute_scores(
     y_train, yhat_train, y_test, yhat_test, fit='BFR')
 
 print(msg)
-# print(model.sparsity_analysis()) # only useful when tau_th>0
+#print(model.sparsity_analysis()) # only useful when tau_th>0
 
